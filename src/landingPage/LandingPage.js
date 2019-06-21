091fc6510d453d799a37683b3ae89b398b468ff5
Alex Rolfe <alex.rolfe27@gmail.com>
2019-06-20 22:41:42 -0500

    Gets initial export from Landing Page working in main App.js

---
+++ src/landingPage/LandingPage.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './LandingPage.css';
+
+function LandingPage() {
+  return (
+    <div className="landing-page">
+      <div className="left-form">
+        <svg className="bird-eye-logo">Logo here</svg>
+        <h1>Welcome to Birdseye!</h1>
+      </div>
+      <div className="blue-swoosh">
+        
+      </div>      
+    </div>
+  );
+}
+
+export default LandingPage;
