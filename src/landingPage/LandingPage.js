2c6e0723a529f14fdee7addc9a2d0a878d33fec1
Alex Rolfe <alex.rolfe27@gmail.com>
2019-06-21 05:11:13 -0500

    Adds Vertical orientation

--- src/landingPage/LandingPage.js
+++ src/landingPage/LandingPage.js
@@ -1,47 +1,51 @@
 import React from 'react';
 import './LandingPage.css';
-import { TextField, Button } from '@material-ui/core';
+import { TextField, Button, Grid } from '@material-ui/core';
 
 function LandingPage() {
   return (
     <div className="landing-page">
-      <div className="left-form">
-        {/* <svg className="bird-eye-logo">Logo here</svg> */}
-        <p>LOGO</p>
-        <h1>Welcome to Birdseye!</h1>
-        <form>
-          <TextField 
-            variant="outlined"
-            margin="normal"            
-            label="Email Address" 
-          />
-          <TextField 
-            variant="outlined"
-            margin="normal"            
-            label="Password"
-          />
-          <Button 
-            label="Submit" 
-            type="submit" 
-            variant="contained"            
-          >
-            Sign In
-          </Button>
-          <Button 
-            label="Submit" 
-            type="submit" 
-            variant="contained"            
-          >
-            Create Account
-          </Button>
-          {/* <Link href="#" variant="body2">
-            Forgot password?
-          </Link> */}
-        </form>
-      </div>
-      <div className="blue-swoosh">
-        
-      </div>      
+      <Grid container component="main">
+        <Grid item xs={12} sm={4} md={5} className="left-form">
+          {/* <svg className="bird-eye-logo">Logo here</svg> */}
+          <p>LOGO</p>
+          <h1>Welcome to Birdseye!</h1>
+          <form>
+            <TextField 
+              variant="outlined"
+              margin="normal"
+              fullWidth  
+              name="email"    
+              label="Email Address" 
+            />
+            <TextField 
+              variant="outlined"
+              margin="normal"
+              fullWidth
+              namme="password"
+              label="Password"
+            />
+            <Button 
+              label="Submit" 
+              type="submit" 
+              variant="contained"            
+            >
+              Sign In
+            </Button>
+            <Button 
+              label="Submit" 
+              type="submit" 
+              variant="contained"            
+            >
+              Create Account
+            </Button>
+            {/* <Link href="#" variant="body2">
+              Forgot password?
+            </Link> */}
+          </form>
+        </Grid>
+        <Grid item xs={false} sm={8} md={7} className="blue-swoosh" />
+      </Grid>
     </div>
   );
 }
