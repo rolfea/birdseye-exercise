d2a31af1e5f7e9755a74b5d4bf21ca1c292c4ac7
Alex Rolfe <alex.rolfe27@gmail.com>
2019-06-20 23:42:15 -0500

    Stubs out elements. Adds small amount of styling to separate the two divs

--- src/landingPage/LandingPage.js
+++ src/landingPage/LandingPage.js
@@ -1,12 +1,43 @@
 import React from 'react';
 import './LandingPage.css';
+import { TextField, Button } from '@material-ui/core';
 
 function LandingPage() {
   return (
     <div className="landing-page">
       <div className="left-form">
-        <svg className="bird-eye-logo">Logo here</svg>
+        {/* <svg className="bird-eye-logo">Logo here</svg> */}
+        <p>LOGO</p>
         <h1>Welcome to Birdseye!</h1>
+        <form>
+          <TextField 
+            variant="outlined"
+            margin="normal"            
+            label="Email Address" 
+          />
+          <TextField 
+            variant="outlined"
+            margin="normal"            
+            label="Password"
+          />
+          <Button 
+            label="Submit" 
+            type="submit" 
+            variant="contained"            
+          >
+            Sign In
+          </Button>
+          <Button 
+            label="Submit" 
+            type="submit" 
+            variant="contained"            
+          >
+            Create Account
+          </Button>
+          {/* <Link href="#" variant="body2">
+            Forgot password?
+          </Link> */}
+        </form>
       </div>
       <div className="blue-swoosh">
         
